2cd182b11ef0beb06c3496842033b9dce74c86ac
miri8D <inadvanced8282@gmail.com>
2021-10-14 15:52:33 +0900

    Add basic lights

--- src/script.js
+++ src/script.js
@@ -24,11 +24,17 @@ window.addEventListener('resize', () => {
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-environment.floor.map(x => {scene.add(x)})
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+scene.add(ambientLight)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
+directionalLight.position.set(0, 0.25, 0)
+scene.add(directionalLight)
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.set(0, 1, 0)
 scene.add(camera) 
+
+environment.floor.map(x => {scene.add(x)})
 
 const tick = () =>
 {
