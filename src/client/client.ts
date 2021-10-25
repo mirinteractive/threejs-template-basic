0dce1e91afa39902c4f627d03a0cd5ab15015c6d
miri8D <inadvanced8282@gmail.com>
2021-10-26 00:34:21 +0900

    Add dat gui for environment setting

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import * as dat from 'three/examples/jsm/libs/dat.gui.module'
 import * as environment from './environment'
 import * as objects from './object'
@@ -12,10 +13,16 @@ const sizes = { width: window.innerWidth, height: window.innerHeight}
 
 scene.background = new THREE.Color('#8b9dc3')
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+})
 renderer.setSize(sizes.width, sizes.height)
 renderer.physicallyCorrectLights = true
 renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.toneMappingExposure = 3
 document.body.appendChild(renderer.domElement)
 
 const camera = new THREE.PerspectiveCamera(
@@ -37,15 +44,39 @@ function onWindowResize() {
     render()
 }
 
-const gui = new dat.GUI()
+
+const updateAllMaterials = () => {
+    scene.traverse((child) => {
+        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial){
+            child.castShadow = true
+            child.receiveShadow = true
+        }
+    })
+}
+
 //이거로 애니메이션 poc 진행
 // const baseEnvironment = environment.base.filter(x => {
 //     scene.add(x)
 //     lightFolder.add(x, 'intensity').min(0).max(10).step(0.001).name(x.type)
 //     return x
 // })
+const gui = new dat.GUI()
 const lightFolder = gui.addFolder('Light')
 lightFolder.open()
+const tomeMappingFolder = gui.addFolder('Tone')
+tomeMappingFolder.open()
+
+tomeMappingFolder.add(renderer, 'toneMapping', {
+        No: THREE.NoToneMapping,
+        Linear: THREE.LinearToneMapping,
+        Reinhard: THREE.ReinhardToneMapping,
+        Cineon: THREE.CineonToneMapping,
+        ACESFilmic: THREE.ACESFilmicToneMapping
+    }).onFinishChange(() => {
+        renderer.toneMapping = Number(renderer.toneMapping)
+        updateAllMaterials()
+    })
+tomeMappingFolder.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001)
 environment.base.filter(x => {
     scene.add(x)
     lightFolder.add(x, 'intensity').min(0).max(10).step(0.001).name(x.type)
@@ -55,7 +86,10 @@ environment.base.filter(x => {
     return x
 })
 environment.floor.map(x => {scene.add(x)})
-objects.geometry.map(x=>{scene.add(x)})
+objects.geometry.map(x=>{
+    scene.add(x); 
+    updateAllMaterials();
+})
 
 function animate() {
     requestAnimationFrame(animate)
